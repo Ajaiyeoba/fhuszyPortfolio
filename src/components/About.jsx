b84110897956b897474069dc56631c2b463ae859
Ajaiyeoba <ajaiyeobajibola@gmail.com>
2024-11-21 19:00:51 +0100

    a

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,14 +11,9 @@ function About() {
       <div className="container">
         <div className="bg-sky-400 p-7 rounded-2xl md:p-12  reveal-up">
           <p className=" text-zinc-900 mb-4 md:mb-8 md:text-xl md:max-w-[60ch]">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
-            nemo, rem sequi officiis eum, enim eaque earum temporibus vero
-            consectetur, dicta architecto! Blanditiis laboriosam maxime,
-            provident quia ex fugiat excepturi. Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Corporis non repudiandae provident
-            similique voluptas, voluptatibus dolore labore magni dignissimos
-            itaque animi deleniti obcaecati perferendis beatae sed. Expedita
-            dignissimos et a.
+           Dive into the world of frontend Web Design, Social Media Management, Content Creation,
+           Videography and Graphic Design.
+           Discover the creative projects crafted with HTMLL, CSS Javascript, Wordpress, Canva, Capcut and Corel Draw. 
           </p>
 
           <div className=" flex flex-wrap items-center gap-4 md:gap-7">
