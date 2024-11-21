839a86a313acd4b43dd1d0ad3b9a787412a67b50
Ajaiyeoba <ajaiyeobajibola@gmail.com>
2024-11-21 19:07:24 +0100

    updated clients skill set

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,13 +1,13 @@
 import SkillCard from "./SkillCard";
 
 const skillItem = [
-  { label: "Figma", desc: "Design Tool" },
-  { label: "Javascript", desc: "Interaction" },
-  { label: "React", desc: "Framework" },
-  { label: "Tailwind", desc: "Interface" },
-  { label: "Nodejs", desc: "Backend" },
-  { label: "Express", desc: "Node framework" },
+  { label: "Html", desc: "Web Design" },
+  { label: "Css", desc: "Styling" },
+  { label: "Javascript", desc: "FrInteractionamework" },
   { label: "Wordpress", desc: "CMS" },
+  { label: "Capcut", desc: "Video" },
+  { label: "Canva", desc: "Fliers" },
+  { label: "Corel Draw", desc: "Graphics Design" },
 ];
 
 function Skill() {
@@ -16,19 +16,17 @@ function Skill() {
       <div className="container">
         <h2 className="headline-2 reveal-up">Essential Tools i use</h2>
         <p className="text-zinc-900 reveal=up mt-3 mb-8 max-w-[50ch]">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
-          sunt maxime id sapiente harum libero iste expedita. Cum mollitia illum
-          distinctio quis, accusamus dolorum minus enim minima dolore incidunt
-          reiciendis.
+        Creative Digital Solution: Web Design, Social Media, Content creation, Videograpgy and Graphic Design with Html, Css, Js Wordpress, Canva, Capcut and Corel Draw.
         </p>
 
         <div className="grid gap-3 grid-cols-[repeat(auto-fill,_minmax(250px,_1fr))]">
           {skillItem.map(({ label, desc }, key) => (
-            <SkillCard 
-             key={key}
-             label={label} 
-             desc={desc} 
-             classes="reveal-up" />
+            <SkillCard
+              key={key}
+              label={label}
+              desc={desc}
+              classes="reveal-up"
+            />
           ))}
         </div>
       </div>
